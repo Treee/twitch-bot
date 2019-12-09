6246c76b0a7494c4901855ac7cea8a935c42e73f
Treee <jonathon.lindemann@gmail.com>
2019-12-09 13:30:49 -0700

    adding license and readme

--- build/index.js
+++ build/index.js
@@ -12,9 +12,3 @@ const twitchChatConnectionOptions = {
     ]
 };
 const twitchChatBot = new twitch_websocket_server_1.TwitchWebSocketServer(twitchChatConnectionOptions);
-// import { PubSubWebSocket } from './src/twitch-connectors/pub-sub-websocket';
-// const connector = new PubSubWebSocket();
-// connector.connect();
-// import { HelixApi } from './src/twitch-connectors/helix-api';
-// const helix = new HelixApi();
-// helix.getMostActiveStreamsForGameId(33214);
