d343d73d0f9a18174ba312d5b7bf44bdd2b98e66
Treee <jonathon.lindemann@gmail.com>
2019-12-19 17:27:08 -0700

    more complex aabb collisions

--- src/overlay-widgets/physics-widget/tree-math.ts
+++ src/overlay-widgets/physics-widget/tree-math.ts
@@ -1,6 +1,8 @@
-export enum ClockWiseDirection {
-    ClockWise, CounterClockWise
+export enum ClockDirection {
+    // clockwise, counter clockwise
+    CW, CCW
 }
+
 export class Vector2 {
     values: number[];
     constructor(x: number = 0, y: number = 0) {
@@ -19,6 +21,17 @@ export class Vector2 {
         return Math.sqrt((this.values[0] * this.values[0]) + (this.values[1] * this.values[1]));
     }
 
+    multiplyScalar(scalar: number): Vector2 {
+        return new Vector2(this.values[0] * scalar, this.values[1] * scalar);
+    }
+
+    divideScalar(scalar: number): Vector2 {
+        if (scalar === 0) {
+            throw new Error('Cannont divide by 0');
+        }
+        return new Vector2(this.values[0] / scalar, this.values[1] / scalar);
+    }
+
     normalize(): Vector2 {
         const magnitude = this.magnitude();
         if (magnitude > 0) {
@@ -28,10 +41,10 @@ export class Vector2 {
         }
     }
 
-    normal(direction: ClockWiseDirection): Vector2 {
-        if (direction === ClockWiseDirection.ClockWise) {
+    normal(direction: ClockDirection): Vector2 {
+        if (direction === ClockDirection.CW) {
             return new Vector2(this.values[1], -this.values[0]);
-        } else if (direction === ClockWiseDirection.CounterClockWise) {
+        } else if (direction === ClockDirection.CCW) {
             return new Vector2(-this.values[1], this.values[0]);
         } else {
             throw new Error('Cannot return normal');
@@ -39,6 +52,32 @@ export class Vector2 {
     }
 
     dot(other: Vector2): number {
-        return ((this.values[0] * other.values[0]) + (this.values[1] * other.values[1]));
+        const aNorm = this.normalize();
+        const oNorm = other.normalize();
+        // console.log(`(${aNorm.values[0]}, ${aNorm.values[1]}) dot (${oNorm.values[0]}, ${oNorm.values[1]})`);
+        return ((aNorm.values[0] * oNorm.values[0]) + (aNorm.values[1] * oNorm.values[1]));
+    }
+
+    crossProduct(other: Vector2): number {
+        const aNorm = this.normalize();
+        const oNorm = other.normalize();
+        return ((aNorm.values[0] * oNorm.values[1]) - (aNorm.values[1] * oNorm.values[0]));
+    }
+
+    angleBetween(other: Vector2): number {
+        return (Math.acos(this.dot(other)) * (180 / Math.PI));
+    }
+
+    checkBoundaries(aVec: Vector2, listOfBoundaries: Vector2[]) {
+        const collidedSides: Vector2[] = [];
+        let colliding = false;
+        listOfBoundaries.forEach((boundary: Vector2) => {
+            // console.log(boundary, aVec.dot(boundary));
+            if (aVec.dot(boundary) < 0) {
+                colliding = true;
+                collidedSides.push(boundary);
+            }
+        });
+        return { colliding: colliding, collidedSides: collidedSides };
     }
 }
