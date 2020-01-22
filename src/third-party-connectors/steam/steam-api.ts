a2da6cb9e1a98ce34e3da93f226538952f8b7428
Treee <jonathon.lindemann@gmail.com>
2020-01-22 14:51:52 -0700

    large rework in moving logic and functionality around

--- src/third-party-connectors/steam/steam-api.ts
+++ src/third-party-connectors/steam/steam-api.ts
@@ -4,50 +4,30 @@ import { PlayerSummary } from "./player-summary";
 export class SteamApi {
     constructor() { }
 
-    private getSteamRequestHeaders(): Headers {
-        const headers = new Headers();
-        headers.append('mode', 'no-cors');
-        return headers;
-    }
-
-    async getAoEJoinLink(apiKey: string, steamUserId: string): Promise<string> {
-        const headers = this.getSteamRequestHeaders();
-        return await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamUserId}`, { headers }).then((response) => {
-
-            return '';
+    // if you are invisible in steam, this will return no lobby
+    async getSteamJoinableLobbyLink(apiKey: string, userId: string) {
+        return this.getPlayerSummaries(apiKey, userId).then((playerSummarys) => {
+            if (playerSummarys.length > 0) {
+                return playerSummarys[0].getJoinableGameLink();
+            }
         }, (error) => {
-            throw new Error(error);
+            console.error('Error', error);
+            return '';
         });
     }
 
-
-    async getPlayerSummaries(apiKey: string, steamUserId: string): Promise<PlayerSummary[]> {
-        const headers = this.getSteamRequestHeaders();
-        return await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamUserId}`).then((response) => {
-            console.log('steam resposne', response);
-            return [];
+    private async getPlayerSummaries(apiKey: string, steamUserId: string): Promise<PlayerSummary[]> {
+        return await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamUserId}`).then(async (response) => {
+            const data = await response.json();
+            const playerSummariesRaw = data.response.players;
+            const playerSummaries: PlayerSummary[] = [];
+            playerSummariesRaw.forEach((playerSummary: any) => {
+                playerSummaries.push(new PlayerSummary(playerSummary));
+            });
+            return playerSummaries;
         }, (error) => {
             console.error('Error', error);
             return [];
         });
     }
-    // async getTwitchEmotesBySets(clientId: string, setIds: number[]): Promise<TwitchEmote[]> {
-    //     const headers = this.getTwitchRequestHeaders(clientId);
-    //     return await fetch(`https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=${setIds.join(',')}`, { headers }).then(async (response) => {
-    //         let data = await response.json();
-    //         // console.log('emotes by set emotes', data);
-    //         const emoticonSets = data.emoticon_sets || {};
-    //         const formattedEmotes: TwitchEmote[] = [];
-    //         setIds.forEach((setId: number) => {
-    //             if (emoticonSets[setId]) {
-    //                 emoticonSets[setId].forEach((emote: any) => {
-    //                     formattedEmotes.push(new TwitchEmote(emote.code, emote.emoticon_set, emote.id));
-    //                 });
-    //             }
-    //         });
-    //         return formattedEmotes;
-    //     }, (error) => {
-    //         return [];
-    //     });
-    // }
 }
