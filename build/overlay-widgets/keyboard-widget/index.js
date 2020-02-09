ec9f1fec2e43f2c52e9139eb5c3adfc8b16efa7e
Treee <jonathon.lindemann@gmail.com>
2020-02-09 13:23:43 -0700

    removing build

--- build/overlay-widgets/keyboard-widget/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const keyboard_widget_1 = require("./keyboard-widget");
-const keyboardWidget = new keyboard_widget_1.KeyboardWidget();
-// new KeyboardWidgetClient('ws://localhost:8081', keyboardWidget);
