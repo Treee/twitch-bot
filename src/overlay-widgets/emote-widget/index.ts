7a691b085fe57c793789d0610362b8d58c5fa582
Treee <jonathon.lindemann@gmail.com>
2020-04-10 16:21:47 -0600

    adding memb branch

--- src/overlay-widgets/emote-widget/index.ts
+++ src/overlay-widgets/emote-widget/index.ts
@@ -12,25 +12,21 @@ emoteWidgetConfig.setConfigFrom(window.location.search.substring(1));
 const emoteFactory = new EmoteFactory();
 const emoteWidget = new EmoteWidget(emoteWidgetConfig, emoteFactory);
 
-const twitchDefault = 0;
-const textEmojiDefault = 42;
-const amazonPrimeDefault = 19194;
-const membTier1 = 6112;
-const membTier2 = 24314;
-const membTier3 = 24315;
-const nikeTier1 = 12661;
-const thunderTier1 = 135189;
+// const twitchDefault = 0;
+// const textEmojiDefault = 42;
+// const amazonPrimeDefault = 19194;
 
-const emoteSetIds = [twitchDefault, textEmojiDefault, amazonPrimeDefault, membTier1, membTier2, membTier3, nikeTier1, thunderTier1];
+
+// const emoteSetIds = [twitchDefault, textEmojiDefault, amazonPrimeDefault, membTier1, membTier2, membTier3];
 
 Promise.all([
     twitchApiV5.getTwitchEmotes(emoteWidgetConfig.clientId, emoteWidgetConfig.channel),
-    twitchApiV5.getTwitchEmotesBySets(emoteWidgetConfig.clientId, emoteSetIds),
-    twitchApiV5.getBttvEmotesByChannel(emoteWidgetConfig.channel),
-    twitchApiV5.getGlobalBttvEmotes()
+    // twitchApiV5.getTwitchEmotesBySets(emoteWidgetConfig.clientId, emoteSetIds),
+    // twitchApiV5.getBttvEmotesByChannel(emoteWidgetConfig.channel),
+    // twitchApiV5.getGlobalBttvEmotes()
 ]).then((values) => {
     // emoteWidget.twitchSubBadges = values[0].subBadges;
-    emoteFactory.masterEmoteList = emoteFactory.masterEmoteList.concat(values[0]).concat(values[1]).concat(values[2]).concat(values[3]);
+    emoteFactory.masterEmoteList = emoteFactory.masterEmoteList.concat(values[0]);//.concat(values[1]).concat(values[2]).concat(values[3]);
 }).then(() => {
     if (!emoteWidgetConfig.botMode) {
         emoteWidget.startSimulation();
@@ -44,7 +40,7 @@ Promise.all([
     }
 }).then(() => {
     if (emoteWidgetConfig.botMode) {
-        new EmoteWidgetClient('wss://www.itsatreee.com/emotewidgettree', emoteWidget);
+        new EmoteWidgetClient('wss://www.itsatreee.com/emotewidgetmembtv', emoteWidget);
         twitchApiV5.test(emoteWidgetConfig.clientId);
         emoteWidget.startSimulation();
     }
