19c2fa5ab6ddd1f1722da655ea9a77c565b83c9a
Treee <jonathon.lindemann@gmail.com>
2020-07-29 18:40:24 -0600

    adding raid message for raids and hosts

--- src/third-party-connectors/twitch/socket-message-enum.ts
+++ src/third-party-connectors/twitch/socket-message-enum.ts
@@ -2,5 +2,5 @@ export enum SocketMessageEnum {
     FoundEmotes, CheckEmoteCache, EmoteCodes, HandleInput, HookInput, PressedKeys,
     EmoteOnlyModeActive, EmoteOnlyModeDisabled, ChatCleared, Bits, Banned,
     FirstTimeSubscription, MysteryGiftSubscription, GiftSubscription, ReSubscription,
-    GiftSubscriptionUpgrade, MysteryGiftSubscriptionUpgrade
+    GiftSubscriptionUpgrade, MysteryGiftSubscriptionUpgrade, Raided
 }
